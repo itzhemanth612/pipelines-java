28e845b3693939ea249c062daa39bd7bc9c3db0a
kumar <itzhemanth612@gmail.com>
2023-09-12 09:31:15 +0530

    commit

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -9,6 +9,6 @@ public class Demo {
 
         System.out.println("I am not covered");
         System.out.println("I am not covered with it");
-        System.out.println("I am not covered on it");
+        System.out.println("I am not covered on");
     }
 }
